0486ea8fdff904b890bc76306792065c90915907
Bill Weber <wweber@wlweber.com>
2023-02-21 20:58:03 -0600

    Clean-Up

--- enroll_sections.sh
+++ enroll_sections.sh
@@ -4,19 +4,21 @@ INPUT_FILE=$1
 
 IFS=','
 
-while read URL SUBACCT_ID EMAIL COURSE_CODE SECTION_NAME ENROLLMENT_TYPE
+while read URL TOKEN SUBACCT_ID EMAIL COURSE_CODE SECTION_NAME ENROLLMENT_TYPE
 do
 
-USER_ID=$(curl -s "https://$URL.instructure.com/api/v1/accounts/$SUBACCT_ID/users?search_term=$EMAIL" \
-     -H 'Authorization: <token>' | /usr/local/bin/jq '.[].id')
+USER_ID=$(curl -s "https://"$URL".instructure.com/api/v1/accounts/"$SUBACCT_ID"/users?search_term="$EMAIL \
+     -H 'Authorization: Bearer '$TOKEN | /usr/local/bin/jq '.[].id')
 
-C_ID=$(curl -s "https://$URL.instructure.com/api/v1/accounts/$SUBACCT_ID/courses?search_term=$COURSE_CODE" \
-     -H 'Authorization: <token>' | /usr/local/bin/jq '.[].id')
+C_ID=$(curl -s "https://"$URL".instructure.com/api/v1/accounts/"$SUBACCT_ID"/courses?search_term="$COURSE_CODE \
+     -H 'Authorization: Bearer '$TOKEN | /usr/local/bin/jq '.[].id')
 
-SECTION_ID=$(curl "https://uws.instructure.com/api/v1/courses/289542/sections" \
-	-H 'Authorization: <token>' | /usr/local/bin/jq '.[].id')
+SECTION_ID=$(curl "https://"$URL".instructure.com/api/v1/courses/"$C_ID"/sections" \
+	-H 'Authorization: Bearer '$TOKEN | /usr/local/bin/jq '.[].id')
 
-curl -X "POST" "https://$URL.instructure.com/api/v1/sections/$SECTION_ID/enrollments?enrollment%5Buser_id%5D=$USER_ID&enrollment%5Btype%5D=$ENROLLMENT_TYPE" \
-     -H 'Authorization: <token>' 
+curl -H 'Authorization: Bearer '$TOKEN "https://"$URL".instructure.com/api/v1/sections/"$SECTION_ID"/enrollments" \
+     -X "POST" \
+     -F 'enrollment[user_id]'=$USER_ID \
+     -F 'enrollment[type]='$ENROLLMENT_TYPE
 
 done < $INPUT_FILE
