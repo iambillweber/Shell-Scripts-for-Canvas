f91b2c6192fd6bd39b2b4f848d49ae785d3d28ca
Bill Weber <wweber@wlweber.com>
2023-03-02 16:16:24 -0600

    Update enroll.sh

--- enroll.sh
+++ enroll.sh
@@ -4,17 +4,25 @@ INPUT_FILE=$1
 
 IFS=','
 
-while read URL TOKEN SUBACCT_ID EMAIL COURSE_CODE ENROLLMENT_TYPE
+ACCESS_TOKEN="your_access_token"
+CANVAS_URL="your_canvas_url"
+API_ENDPOINT="${CANVAS_URL}/api/vi"
+
+
+while read SUBACCT_ID EMAIL COURSE_CODE ENROLLMENT_TYPE
 do
 
-USER_ID=$(curl -s "https://"$URL".instructure.com/api/v1/accounts/"$SUBACCT_ID"/users?search_term="$EMAIL \
-     -H 'Authorization: Bearer '$TOKEN | /usr/local/bin/jq '.[].id')
+USER_ID=$(curl --request GET  \
+	--url "${API_ENDPOINT}/accounts/${SUBACCT_ID}/users?search_term=${EMAIL}" \
+    --header "Authorization: Bearer ${ACCESS_TOKEN}" | /usr/local/bin/jq '.[].id')
 
-C_ID=$(curl -s "https://"$URL".instructure.com/api/v1/accounts/"$SUBACCT_ID"/courses?search_term="$COURSE_CODE \
-     -H 'Authorization: Bearer '$TOKEN | /usr/local/bin/jq '.[].id')
+C_ID=$(curl --request GET \
+	--url "${API_ENDPOINT}/accounts/${SUBACCT_ID}/courses?search_term=${COURSE_CODE}" \
+    --header "Authorization: Bearer ${ACCESS_TOKEN}" | /usr/local/bin/jq '.[].id')
 
-curl -H 'Authorization: Bearer '$TOKEN "https://$URL.instructure.com/api/v1/courses/$C_ID/enrollments" \
-     -X "POST" \
+curl --request POST \
+	--header "Authorization: Bearer ${TOKEN}" \
+	--url "${API_ENDPOINT}/courses/$C_ID/enrollments" \
      -F "enrollment[user_id]=$USER_ID" \
      -F "enrollment[type]=$ENROLLMENT_TYPE"
 
